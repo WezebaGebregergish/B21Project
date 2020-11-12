a6e494928f652863e50d05bb243a1ac76a5e8d93
wezebaGebregergish <wezeogbay@gmail.com>
2020-11-11 22:30:40 -0500

    project is already shared in remote repo.

--- src/Hellow.java
+++ src/Hellow.java
@@ -2,6 +2,10 @@ public class Hellow {
     public static void main(String[] args) {
         System.out.println("great");
 //more stuff
-
+// I have already published on Github.com
+// now I am ready to make more commits
+// but do I need to share again???
+// Absolutely No
+//It's time to commit and just push
     }
 }
